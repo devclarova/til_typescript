a152fec533ee6aed2495243bacda33a5c8e97833
dev.clarova <dev.clarova@gmail.com>
2025-07-16 09:44:26 +0900

    [docs] 데이터 타입 정리

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-type IdolType = {
-  name: string;
-  age: string;
+interface IdolType {
+  readonly name: string; // 읽기 전용
+  age?: string; // 옵셔널
   year: number;
-};
+}
 
 let bts: IdolType = {
-  name: "bts",
-  age: "20e대",
+  name: "BTS",
   year: 2020,
 };
+
+bts.name = "홍길동"; // Error변경 불가
+bts.year = 2000;
